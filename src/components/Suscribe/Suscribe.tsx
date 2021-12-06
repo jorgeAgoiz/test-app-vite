b7bfb4d7e23e39f5b3fe6902ae0c17cf5d57536c
jorgeAgoiz <jorgeagoiz@gmail.com>
2021-12-06 08:40:21 +0100

    Working on suscribe section

--- src/components/Suscribe/Suscribe.tsx
+++ src/components/Suscribe/Suscribe.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, Row } from 'react-bootstrap'
+import { useLocation } from 'wouter'
 import { getKey } from '../../services/getKey'
 
-const Suscribe = ():JSX.Element => {
+interface State {
+	suscribeMsg: string | null;
+	textColor: string | null;
+}
+
+const Suscribe = (): JSX.Element => {
+	const [ suscribeMsg, setSuscribeMsg ] = useState<State['suscribeMsg']>()
+	const [ textColor, setTextColor ] = useState<State['textColor']>()
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [ location, setLocation ] = useLocation()
 
 	const onHandleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
 		evt.preventDefault()
 		const email: string = evt.currentTarget.email.value
 		const signed = await getKey(email)
-		console.log(signed)
-		/* Depende de la respuesta hacemos una cosa o otra */
+		const statusCode: string | null = localStorage.getItem('status_code') 
+		
+		if (statusCode === '201') {
+			setTextColor('text-success')
+			setTimeout(() => {
+				return setLocation('/')
+			}, 2000)
+		} else {
+			setTextColor('text-danger')
+		}
+		setSuscribeMsg(signed.message)
 	}
 
 	return (
@@ -28,6 +47,9 @@ const Suscribe = ():JSX.Element => {
 						Enviar
 					</Button>
 				</Form>
+			</Row>
+			<Row className={`mt-5 mb-5 justify-content-center ${textColor}`}>
+				{suscribeMsg}
 			</Row>
 		</>
 	)
--- src/services/getKey.ts
+++ src/services/getKey.ts
@@ -1,10 +1,18 @@
-export const getKey = (email: string):Promise<Response> => {
+interface Data {
+	message?: string,
+	response?: string
+}
+
+export const getKey = (email: string): Promise<Data> => {
 	return fetch('https://quiz-questions-rest-api.herokuapp.com/apikey', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({email})
 	})
-		.then(response => response.json())
+		.then(response => {
+			localStorage.setItem('status_code', `${response.status}`)
+			return response.json()
+		})
 		.then(data => data)
 		.catch(err => err)
 }
